043ddfe5e6061a56456cd5215150324d7de5ab23
Dean Jürges <s67337@beuth-hochschule.de>
2018-01-12 14:09:55 +0100

    Script hinzugefuegt, das das Label zur Kamera dreht

---
+++ Assets/3.Hololens/Scripts/LabelCameraInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+
+public class LabelCameraInteraction : MonoBehaviour {
+
+    private Vector3 camPosition;
+    private GameObject Label;
+
+    // Use this for initialization
+    void Start ()
+	{
+        Label = this.gameObject;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        camPosition = Camera.main.transform.position;
+        Label.transform.LookAt(camPosition);
+    }
+}
