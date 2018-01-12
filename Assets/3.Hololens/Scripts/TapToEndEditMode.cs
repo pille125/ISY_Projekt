c47d1db2a98e4e5db35d56b978ef221cfe7d8174
Dean Jürges <s67337@beuth-hochschule.de>
2018-01-12 14:02:01 +0100

    Manipulation Bar scripts hinzugefuegt. Hier gilt das gleiche wie beim letzten commit.

---
+++ Assets/3.Hololens/Scripts/TapToEndEditMode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+
+public class TapToEndEditMode : MonoBehaviour, IInputClickHandler
+{
+    // When Object is Clicked than is manipulating possible
+
+    public GameObject ManipulationBar;
+    public GameObject InfoLabel;
+    public GameObject MainObject;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        // Make manipulating object visible
+        ManipulationBar.SetActive(false);
+        InfoLabel.SetActive(true);
+        MainObject.GetComponent<LineRenderer>().enabled = true;
+    }
+}
