d95b899a3037b8dca598247be1a00df62833cf73
Dean Jürges <s67337@beuth-hochschule.de>
2018-01-13 14:55:56 +0100

    FingerTapToSelect angepasst und ToolParent hinzugefuegt damit jetzt jedes Objekt angewaehlt werden kann, dass das Script FingerTapToSelect hat. Ich habe versucht es abwärtskompatibel zu lassen. Translation Tool fbx ebenfalls hinzugefuegt

--- Assets/3.Hololens/Scripts/FingerTapToSelect.cs
+++ Assets/3.Hololens/Scripts/FingerTapToSelect.cs
@@ -7,20 +7,31 @@ using HoloToolkit.Unity.InputModule;
 public class FingerTapToSelect : MonoBehaviour, IInputClickHandler
 {
     // When Object is clicked, then manipulating is possible
-    public GameObject ManipulationCube;
+    public GameObject Tools;
     public GameObject InfoLabel;
 
     // Use this for initialization
     void Start ()
 	{
-        ManipulationCube.SetActive(false);
+		Tools.SetActive(false);
     }
 		
     public void OnInputClicked(InputClickedEventData eventData)
     {
-        // Make manipulating object visible
-        ManipulationCube.SetActive(true);
-        InfoLabel.SetActive(false);
-        this.GetComponent<LineRenderer>().enabled = false;
+        // Make tools visible
+		Tools.SetActive(true);
+
+		// Set active Object in Tools
+		ToolParent tp = Tools.GetComponent<ToolParent>();
+		if (tp)
+			tp.setManipulatingObject(gameObject);
+
+		// Activate info label
+		if (InfoLabel)
+		{ // Ich habe das hier erstmal fuer mein Level ohne Info Labels abefangen
+			// Kann in Zukunft vielleicht wieder entfernt werden
+			InfoLabel.SetActive(false);
+			this.GetComponent<LineRenderer>().enabled = false;
+		}
     }
 }
---
+++ Assets/3.Hololens/Scripts/ToolParent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolParent : MonoBehaviour {
+
+	public GameObject manipulatingObject;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Update position and rotation if an object is selected
+		if (manipulatingObject)
+		{
+			transform.position = manipulatingObject.transform.position;
+			transform.LookAt (Camera.main.transform);
+		}
+	}
+
+	public void setManipulatingObject (GameObject mo)
+	{
+		manipulatingObject = mo;
+	}
+}
